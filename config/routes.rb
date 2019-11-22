5b220c31d78de05d76367fa101729fa7d037c430
EC2 Default User <ec2-user@ip-172-31-90-200.ec2.internal>
2019-11-22 15:30:22 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Rails.application.routes.draw do
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
     
-    get 'signup', to: 'users#show'
-    resources :users, only: [:index, :show, :new, :create]
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :new, :create] do
+        member do
+            get :followings
+            get :followers
+        end
+    end
     
     resources :microposts, only: [:create, :destroy]
+    resources :relationships, only:[:create, :destroy]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
